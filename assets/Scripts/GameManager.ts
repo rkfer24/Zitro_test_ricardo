fcedc382e7af0fac88234985cf060c5f21424c0a
Rkfer24 <ricardoferminhernandez@gmail.com>
2023-04-28 13:33:57 +0200

    GameManager cambio de escena & Test funcion asincronica
    GameManager para controlar escenas y estados del juego. Test funciones asincronicas TypeScript para simular carga de recursos

---
+++ assets/Scripts/GameManager.ts
@@ -0,0 +1,42 @@
+
+import { _decorator, Component, Node, game, director, } from 'cc';
+//añado el espacio de nombre "game" para controlar estados de juego, director para controlar Scenes.
+//
+const { ccclass, property } = _decorator;
+
+@ccclass('GameManager')
+export class GameManager extends Component {
+
+    onLoad() {
+
+       
+        game.addPersistRootNode(this.node); //Nodo en persistente para tener control sobre el juego y el cambio de escenas
+
+
+        function iniciarTimer(milisegundos) {
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    resolve('NivelCargado');
+                }, milisegundos);
+            });
+        }
+
+        async function cuentaAtrasCarga() {
+
+            
+            console.log('CargandoNivel'); //Empiezo a Cargar el Nivel
+            director.preloadScene("Menu");  //Pruebo de hacer un preload de la Scene
+
+            const result = await iniciarTimer(4000);
+            console.log(result);
+
+            director.loadScene("Menu");
+
+        }
+
+        cuentaAtrasCarga(); //prueba funciones asincronicas y carga de escenas
+
+    }
+
+
+}
