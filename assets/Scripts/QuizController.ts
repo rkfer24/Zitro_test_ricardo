8fff08409b54debb18940cd27dffb9221c2f2e0d
Rkfer24 <ricardoferminhernandez@gmail.com>
2023-04-29 17:33:13 +0200

    QuizController.ts con boton a Menu

---
+++ assets/Scripts/QuizController.ts
@@ -0,0 +1,29 @@
+import { _decorator, Component, Node, find } from 'cc';
+import { GameManager } from './GameManager';
+const { ccclass, property } = _decorator;
+
+ 
+@ccclass('QuizController')
+export class QuizController extends Component {
+
+    @property({
+        type: GameManager
+    })
+    gameManager: GameManager = null!;
+
+    start = () => {
+
+
+        const gameManagerNode = find("GameManager");
+        this.gameManager = gameManagerNode.getComponent("GameManager");
+
+    }
+
+    goHome() {
+        this.gameManager.changeScene("Menu");
+
+    }
+
+
+}
+
