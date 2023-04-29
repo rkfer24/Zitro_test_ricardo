b7a930ef88f9bef7813b727e49fbb549c1ccacf1
Rkfer24 <ricardoferminhernandez@gmail.com>
2023-04-29 13:04:26 +0200

    Funcion updateClock llamada desde el update

--- assets/Scripts/Home.ts
+++ assets/Scripts/Home.ts
@@ -1,28 +1,56 @@
-import { _decorator, Component, Node } from 'cc';
+import { _decorator, Component, Node, Label } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Home')
 export class Home extends Component {
 
-    onLoad() {
+    @property({
+        type: Label
+    })
+    labelTime: Label = null!;
+
+    private timer = 0;
+
+
+    updateClock= () => {
 
         const xhr = new XMLHttpRequest();
         const url = "http://worldtimeapi.org/api/timezone/Europe/Madrid";
 
-        xhr.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200) {
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4 && xhr.status === 200) {
                 console.log("Resultado POSITIVO");
 
                 let response = JSON.parse(xhr.responseText);
                 let datetime = response.datetime;
 
-                let dateTimeCurrentHour = datetime.substring(datetime.indexOf('T')+1, datetime.indexOf('T')+6);
+                let dateTimeCurrentHour = datetime.substring(datetime.indexOf('T') + 1, datetime.indexOf('T') + 9);
 
                 console.log(dateTimeCurrentHour);
+
+                if (this.labelTime) {
+                    this.labelTime.string = dateTimeCurrentHour;
+                }
             }
         };
 
         xhr.open("GET", url, true);
         xhr.send();
+
+    }
+
+
+    update(deltaTime: number) {
+
+        this.timer += deltaTime;
+
+        if (this.timer > 1) {
+
+            this.updateClock();
+            this.timer = 0;
+        }
+
+
+        
     }
 }
