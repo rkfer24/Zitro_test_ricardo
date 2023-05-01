0af6205397e7fd873e814d7bba71b7475cb19d45
Rkfer24 <ricardoferminhernandez@gmail.com>
2023-05-02 00:40:14 +0200

    Fisher-Yates orden preguntas y respuestas

--- assets/Scripts/QuizController.ts
+++ assets/Scripts/QuizController.ts
@@ -35,19 +35,32 @@ export class QuizController extends Component {
     @property({
         type: String
     })
-
-    //VARIABLES
     pathQuizJSON: string = "";
 
+
+
+    //VARIABLES
     quizJSON = JSON;
 
+    questions; answers;
 
     start = () => {
-
 
         const gameManagerNode = find("GameManager");
         this.gameManager = gameManagerNode.getComponent("GameManager");
 
+    }
+
+    onLoad = () => {
+        assetManager.loadRemote(this.pathQuizJSON, TextAsset, (err, textAsset) => {
+            if (err) {
+                console.log("Error Leyendo JSON");
+                return;
+            }
+
+            this.quizJSON = textAsset.json;
+            this.beginQuiz();
+        });
     }
 
     goHome() {
@@ -55,28 +68,64 @@ export class QuizController extends Component {
 
     }
 
-    onLoad = () => {
 
-        assetManager.loadRemote(this.pathQuizJSON, TextAsset, (err, textAsset) => {
-            if (err) {
-                console.log("error");
-                return;
-            }
-
-            this.quizJSON = textAsset.json;
-            this.beginQuiz();
-        });
-
-    }
 
     beginQuiz = () => {
-        
-        this.labelQuestion.string = this.quizJSON.preguntas[0].Pregunta;
-        this.labelAnswer1.string = this.quizJSON.preguntas[0].Respuestas[0];
-        this.labelAnswer2.string = this.quizJSON.preguntas[0].Respuestas[1];
-        this.labelAnswer3.string = this.quizJSON.preguntas[0].Respuestas[2];
-        
+
+        this.questions = this.quizJSON;
+        this.questions.preguntas = this.shuffle(this.questions.preguntas);
+        this.showQuestion();
     }
+
+    shuffle(questions: any[]) { // Algoritmo Fisher–Yates randomizar
+
+        let m = questions.length;
+        let t: any;
+        let i: number;
+
+        // Mientras quedan preguntas (m)
+        while (m) {
+            // Cogemos una random && m--
+            i = Math.floor(Math.random() * m--);
+
+            // Y la cambiamos por el elemento actual
+            t = questions[m];
+            questions[m] = questions[i];
+            questions[i] = t;
+
+            // Mezclamos tmb las respuestas para la pregunta actual
+            let answers = questions[i].Respuestas;
+
+            let n = answers.length;
+            let u: any;
+            let j: number;
+
+
+            while (n) {
+
+                // Cogemos una random && n--
+                u = Math.floor(Math.random() * n--);
+
+                j = answers[n];
+                answers[n] = answers[u];
+                answers[u] = j;
+            }
+        }
+
+        return questions;
+    }
+
+    showQuestion() {
+
+        console.log(this.questions.preguntas[0].Pregunta);
+        console.log(this.questions.preguntas[0].Respuestas[0] + this.questions.preguntas[0].Respuestas[1] + this.questions.preguntas[0].Respuestas[2]);
+
+        this.labelQuestion.string = this.questions.preguntas[0].Pregunta;
+        this.labelAnswer1.string = this.questions.preguntas[0].Respuestas[0];
+        this.labelAnswer2.string = this.questions.preguntas[0].Respuestas[1];
+        this.labelAnswer3.string = this.questions.preguntas[0].Respuestas[2];
+    }
+
 
     quizResponse(response: EventTarget) {
 
@@ -87,12 +136,14 @@ export class QuizController extends Component {
 
         } else {
 
-            console.log("Incorrect Answer");
+            console.log("Incorrect Answer");    
 
         }
 
-
     }
+
+
+
 
 }
 
