02b19c60e8fb3fdbede8d85c67e9feae1b36355a
Rkfer24 <ricardoferminhernandez@gmail.com>
2023-05-03 11:26:26 +0200

    WIP cambiar iconos Rodillos por lista
    Cambiar iconos separando logica y sprites

--- assets/Scripts/SlotMachine.ts
+++ assets/Scripts/SlotMachine.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node, Animation } from 'cc';
+import { _decorator, Component, Node, Animation, SpriteFrame, Sprite } from 'cc';
 const { ccclass, property } = _decorator;
 
 
@@ -11,13 +11,85 @@ export class SlotMachine extends Component {
     })
     animationRodillo: Animation = null!;
 
+    @property({
+        type: Node
+    })
+    rodilloLeftNode: Node = null!;
+
+    @property({
+        type: Node
+    })
+    rodilloMidNode: Node = null!;
+
+    @property({
+        type: Node
+    })
+    rodilloRightNode: Node = null!;
+
+    @property({
+        type: SpriteFrame
+    })
+    BigWin: SpriteFrame = null!;
+
+    @property({
+        type: SpriteFrame
+    })
+    Fresa: SpriteFrame = null!;
+
+    @property({
+        type: SpriteFrame
+    })
+    Siete: SpriteFrame = null!;
+
+    @property({
+        type: SpriteFrame
+    })
+    Limon: SpriteFrame = null!;
+
+    @property({
+        type: SpriteFrame
+    })
+    Mora: SpriteFrame = null!;
+
+    @property({
+        type: SpriteFrame
+    })
+    SlotMachine: SpriteFrame = null!;
 
     start () {
-        // [3]
+
+        //this.animationRodillo.play("rodilloRoll");
+
+        this.loadRodillos();
+
+    }
+
+    loadRodillos() {
+
+        const rodilloLeft = ["BigWin", "Fresa", "Siete", "Limon", "Mora", "SlotMachine"];
+        const rodilloMid = ["Mora", "BigWin", "Siete", "SlotMachine", "Fresa", "Limon"];
+        const rodilloRight = ["Limon", "Mora", "Siete", "BigWin", "SlotMachine", "Fresa"];
+
+        const rodillosLogic = [rodilloLeft, rodilloMid, rodilloRight];
+        const rodillosNodes = [this.rodilloLeftNode, this.rodilloMidNode, this.rodilloRightNode]
+
+        for (var j = 0; j < rodillosNodes.length; j++) {
+
+            for (var i = 0; i < rodillosLogic[0].length; i++) {
+
+                const icon = rodillosLogic[j][i];
+
+                console.log(icon);
+                rodillosNodes[j].getChildByName(i.toString()).getComponent(Sprite).spriteFrame = this.[icon];
 
 
 
-        this.animationRodillo.play("rodilloRoll");
+
+                //this.rodilloLeftNode.getChildByName(i.toString()).getComponent(Sprite).spriteFrame = this[icon];
+            }
+        }
+
+
     }
 
 
